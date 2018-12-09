72de7dbf466dde239321a610f1a937860a3ad438
lauraferrandof <lauraferrandof@gmail.com>
2018-12-09 11:16:33 +0100

    Start JS

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,2 +1,10 @@
 'use strict';
 
+const userSearchEl = document.querySelector('.user-nickname');
+
+const submitBtnEl = document.querySelector('.submit-btn');
+
+console.log('submitBtnEl: ', submitBtnEl);
+
+// When the button is clicked
+    //a. 
