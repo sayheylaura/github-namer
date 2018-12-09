34d3b06f1041ee9f05c63f8a97fe88923152641c
lauraferrandof <lauraferrandof@gmail.com>
2018-12-09 17:40:42 +0100

    Finish function -- it worksgit add .

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -4,34 +4,40 @@ const userSearchEl = document.querySelector('.user-nickname');
 
 const submitBtnEl = document.querySelector('.submit-btn');
 
+const nameListEl = document.querySelector('.name-list');
+
 // When the button is clicked
 function handleBtnClick (event) {
-    // Prevent the default behaviour of a submit button in a form
+    // Prevent the default behaviour of a submit button in a form (else it will refresh the page)
     event.preventDefault();
 
-    // a. Pick the nickname written in input (input value)
+    // a. Pick the nickname written in the search bar (input value)
     const userNickname = userSearchEl.value;
 
-    // b. Fetch GitHub API
+    // b. Fetch GitHub user's API
     fetch(`https://api.github.com/users/${userNickname}`)
     .then(response => response.json())
     .then(data => {
-        console.log(data);
 
         // c. Access full name in GitHub API
         const userFullName = data.name;
-        console.log(userFullName);
 
         // d. Select first name only (without last name and/or spaces)
         const userFullNameArr = userFullName.split(' ');
-        console.log(userFullNameArr);
 
         const userFirstName = userFullNameArr[0];
-        console.log(userFirstName);
-    })
 
-    // e. Paint in HTML a <ol>, each letter of the name must be in a <li>
+        // e. Paint in HTML each letter of the first name as a <li> in a <ol>
+        const userFirstNameArr = userFirstName.split('');
+
+        let nameListContent = '';
+
+        for (let i = 0; i < userFirstNameArr.length; i++) {
+            nameListContent += `<li>${userFirstNameArr[i]}</li>`;
+        }
+
+        nameListEl.innerHTML = nameListContent;
+    })
 }
-    
 
 submitBtnEl.addEventListener('click', handleBtnClick);
