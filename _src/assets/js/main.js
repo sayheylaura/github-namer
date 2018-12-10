ac9657d55a956999bd2abaa63d51566845c140ce
lauraferrandof <lauraferrandof@gmail.com>
2018-12-10 13:39:37 +0100

    Add throw when the user doesn't exist

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -7,38 +7,46 @@ const submitBtnEl = document.querySelector('.submit-btn');
 const nameListEl = document.querySelector('.name-list');
 
 // When the button is clicked
-function handleBtnClick (event) {
-    // Prevent the default behaviour of a submit button in a form (else it will refresh the page)
-    event.preventDefault();
+function handleBtnClick(event) {
+  // Prevent the default behaviour of a submit button in a form (else it will refresh the page)
+  event.preventDefault();
 
-    // a. Pick the nickname written in the search bar (input value)
-    const userNickname = userSearchEl.value;
+  // a. Pick the nickname written in the search bar (input value)
+  const userNickname = userSearchEl.value;
 
-    // b. Fetch GitHub user's API
-    fetch(`https://api.github.com/users/${userNickname}`)
-    .then(response => response.json())
+  // b. Fetch GitHub user's API
+  fetch(`https://api.github.com/users/${userNickname}`)
+    .then(response => {
+      if (!response.ok) {
+        throw (response);
+      }
+      return response.json();
+    })
     .then(data => {
 
-        // c. Access full name in GitHub API
-        const userFullName = data.name;
+      // c. Access full name in GitHub API
+      const userFullName = data.name;
 
-        // d. Select first name only (without last name and/or spaces)
-        const userFullNameArr = userFullName.split(' ');
+      // d. Select first name only (without last name and/or spaces)
+      const userFullNameArr = userFullName.split(' ');
 
-        const userFirstName = userFullNameArr[0];
+      const userFirstName = userFullNameArr[0];
 
-        // e. Paint in HTML each letter of the first name as a <li> in a <ol>
-        const userFirstNameArr = userFirstName.split('');
+      // e. Paint in HTML each letter of the first name as a <li> in a <ol>
+      const userFirstNameArr = userFirstName.split('');
 
-        let nameListContent = '';
+      let nameListContent = '';
 
-        for (let i = 0; i < userFirstNameArr.length; i++) {
-            nameListContent += `<li class="list-item">${userFirstNameArr[i]}</li>`;
-        }
+      for (let i = 0; i < userFirstNameArr.length; i++) {
+        nameListContent += `<li class="list-item">${userFirstNameArr[i]}</li>`;
+      }
 
-        nameListEl.innerHTML = nameListContent;
+      nameListEl.innerHTML = nameListContent;
     })
-    .catch(error => console.log(`An error has occurred: ${error}`));
+    .catch(error => {
+      console.log(`An error has occurred: ${error}`);
+      nameListEl.innerHTML = 'Oh, no! It seems this user doesn\'t exist :(';
+    });
 }
 
 submitBtnEl.addEventListener('click', handleBtnClick);
